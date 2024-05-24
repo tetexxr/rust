65b57745e8eff445b4819135054f7c45f60084c3
Xavier Xiqués <xavier.xiques@gmail.com>
2024-05-24 18:02:26 +0200

    Ownership in Rust

---
+++ 5_ownership/src/main.rs
@@ -0,0 +1,162 @@
+fn main() {
+    let mut s = String::from("hello");
+    s.push_str(", world!"); // push_str() appends a literal to a String
+    println!("{}", s); // This will print `hello, world!`
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{s2}");
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}, world!", s1);
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+}
+
+fn ownership_and_functions() {
+    let s = String::from("hello"); // s comes into scope
+
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
+
+    let x = 5; // x comes into scope
+
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it's okay to still
+                   // use x afterward
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
+
+fn return_values_and_scope() {
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
+
+    let s2 = String::from("hello");     // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2);  // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
+} // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+  // happens. s1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
+
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
+
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
+
+fn pass_variable_but_not_ownership() {
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
+}
+
+fn references_and_borrowing() {
+    let s1 = String::from("hello");
+
+    // We call the action of creating a reference borrowing
+    let len = calculate_length_bt_reference(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length_bt_reference(s: &String) -> usize {
+    s.len()
+}
+
+fn this_value_cannot_be_changed(some_string: &String) {
+    // this_value_cannot_be_changed(%s)
+    some_string.push_str(", world");
+}
+
+fn mutable_references() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn string_slices() {
+    let s = String::from("hello world");
+    let len = s.len();
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    // these are equal
+    let slice = &s[0..2];
+    let slice = &s[..2];
+
+    // these are also equal
+    let slice = &s[3..len];
+    let slice = &s[3..];
+}
+
+fn first_word_with_slices(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn other_slices() {
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+}
