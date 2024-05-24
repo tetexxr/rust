6f12df80461f0f370a0306e25f94d746e028b4b4
Xavier Xiqués <xavier.xiques@gmail.com>
2024-05-24 18:02:57 +0200

    Structs

---
+++ 6_structs/src/main.rs
@@ -0,0 +1,67 @@
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+
+    // Note that the struct update syntax uses = like an assignment; this is because it moves the data
+
+    let user1 = build_user(String::from(""), String::from(""));
+    // Using struct update syntax:
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
+fn build_user_field_init_shorthand(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn struct_without_named_fields() {
+    // called tuple structs
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
